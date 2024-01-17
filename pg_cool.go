c572bb32e6319b69851ca9250a9e0be5885ad5ec
Viktor Degtyarev <scorp249@gmail.com>
2024-01-17 11:57:44 +0300

    fix connection string

--- pg_cool.go
+++ pg_cool.go
@@ -27,7 +27,7 @@ type Userdata struct {
 
 func openConnection() (*sql.DB, error) {
 	conn := fmt.Sprintf(
-		"host=%s port=%d password=%s dbname=%s sslmode=disable",
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		Hostname,
 		Port,
 		Username,
